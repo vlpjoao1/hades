97fb69e92e0afe02cd3874656691aec08b00fc64
Joao Goncalves <esdeciencia@gmail.com>
2022-12-06 21:15:08 -0400

    Recuperación de contraseña II (Bueno) Video 92

--- app/core/login/forms.py
+++ app/core/login/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from core.user.models import User
+
 
 class ResetPasswordForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
@@ -7,3 +9,18 @@ class ResetPasswordForm(forms.Form):
         'class':'form-control',
         'autocomplete':'off'
     }))
+
+    # Esto se ejecuta cuando haces el form.is_valid()
+    def clean(self):
+        cleaned = super().clean()
+        if not User.objects.filter(username=cleaned['username']).exists():
+            # Mensaje de error customizado por cada validación
+            self._errors['error'] = self._errors.get('error',self.error_class())
+            self._errors['error'].append(f'El usuario *{cleaned["username"]}* no existe')
+            #raise forms.ValidationError(f'El usuario *{cleaned["username"]}* no existe')
+        #si no hay erro retornamos la instancia de cleaned
+        return cleaned
+
+    def get_user(self):
+        username = self.cleaned_data['username']
+        return User.objects.get(username=username)
--- app/core/login/views.py
+++ app/core/login/views.py
@@ -1,15 +1,23 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect
 
 # Create your views here.
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import FormView, RedirectView
 
 from config import settings
 from core.login.forms import ResetPasswordForm
+from core.user.models import User
 
 
 class LoginFormView(LoginView):
@@ -59,10 +67,65 @@ class ResetPasswordView(FormView):
     template_name = 'login/resetpwd.html'
     success_url = reverse_lazy('erp:dashboard')
 
+    @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect(settings.LOGIN_REDIRECT_URL)
         return super().dispatch(request, *args, **kwargs)
+
+    def send_email_reset_pwd(self, user):
+        data = {}
+        try:
+            # Establecemos conexion con el servidor smtp de gmail
+            # Conectamos al servidor
+            mailServer = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
+            """Ehlo es la etapa del protocolo smtp en la que un servidor se presenta entre si. Y verifica
+             que no hay errores en el proceso"""
+            #print(mailServer.ehlo())
+            """TLS sirve para mejorar la seguridad de las conexiones SMTP igual que SSL y evitar hackeos"""
+            mailServer.starttls()
+            #print(mailServer.ehlo())
+            mailServer.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+
+            email_to = user.email
+            # Construimos el mensaje simple
+            # mensaje = MIMEText("""Este es el mensaje de las narices""")
+            # Construimos el mensaje que pueda enviar tambien HTML y archivos
+            mensaje = MIMEMultipart()
+            mensaje['From'] = settings.EMAIL_HOST_USER
+            mensaje['To'] = email_to
+            mensaje['Subject'] = 'Reseteo de contraseña'
+
+            # convertimos un template en un string (esto es una funcion de django) y le enviamos parametros
+            content = render_to_string('login/send_email.html',
+                                       #variables que le pasaremos al template
+                                       {'link_resetpwd':'',
+                                        'link_home':'',
+                                        'user': user
+                                        })
+            # Adjuntamos el archivo y especificamos su tipo
+            mensaje.attach(MIMEText(content, 'html'))
+
+            # Envio del mensaje
+            mailServer.sendmail(settings.EMAIL_HOST_USER,
+                                email_to,
+                                mensaje.as_string())
+        except Exception as e:
+            data['error'] = str(e)
+        return data
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            form = ResetPasswordForm(request.POST)  # self.get_form()
+            if form.is_valid():
+                user = form.get_user()
+                data = self.send_email_reset_pwd(user)
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        # Para serializar los elementos que no sean diccionaros, debes establecer safe=False
+        # Ya que estamos enviando una lista de diccionarios, no un diccionario solo
+        return JsonResponse(data, safe=False)
 
     def form_valid(self, form):
         pass
