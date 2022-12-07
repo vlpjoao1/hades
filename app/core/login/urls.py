b5efbe0bc01bc880cc6333b0be02599565f6c79e
Joao Goncalves <esdeciencia@gmail.com>
2022-12-06 20:05:49 -0400

    Recuperación de contraseña I Video 92

--- app/core/login/urls.py
+++ app/core/login/urls.py
@@ -5,6 +5,8 @@ from core.login.views import *
 
 urlpatterns = [
     path('', LoginFormView.as_view(), name='login'),
+    #from django
     path('logout/', LogoutView.as_view(next_page='accounts:login'), name='logout'),
     #path('logout2/', LogoutRedirectView.as_view(), name='logout'),
+    path('reset/password/', ResetPasswordView.as_view(), name='reset_password'),
 ]
--- app/core/login/views.py
+++ app/core/login/views.py
@@ -4,16 +4,16 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 
-
 # Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import FormView, RedirectView
 
 from config import settings
+from core.login.forms import ResetPasswordForm
 
 
 class LoginFormView(LoginView):
-    template_name = 'login.html'
+    template_name = 'login/login.html'
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -22,12 +22,13 @@ class LoginFormView(LoginView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title']='Iniciar sesión'
+        context['title'] = 'Iniciar sesión'
         return context
+
 
 class LoginFormView2(FormView):
     form_class = AuthenticationForm
-    template_name = 'login.html'
+    template_name = 'login/login.html'
     success_url = reverse_lazy('erp:dashboard')
 
     def dispatch(self, request, *args, **kwargs):
@@ -41,8 +42,9 @@ class LoginFormView2(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title']='Iniciar sesión'
+        context['title'] = 'Iniciar sesión'
         return context
+
 
 class LogoutRedirectView(RedirectView):
     pattern_name = 'accounts:login'
@@ -50,3 +52,24 @@ class LogoutRedirectView(RedirectView):
     def dispatch(self, request, *args, **kwargs):
         logout(request)
         return super().dispatch(request, *args, **kwargs)
+
+
+class ResetPasswordView(FormView):
+    form_class = ResetPasswordForm
+    template_name = 'login/resetpwd.html'
+    success_url = reverse_lazy('erp:dashboard')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        pass
+        # login(self.request, form.get_user())
+        return HttpResponseRedirect(self.success_url)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Reseteo de contraseña'
+        return context
